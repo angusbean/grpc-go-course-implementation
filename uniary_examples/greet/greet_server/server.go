f3c083f0ca192e45aa673ce0513520d489609f49
angusbean <angus.bean.design@gmail.com>
2021-12-27 16:41:48 +1100

    added folder for uniary example

---
+++ uniary_examples/greet/greet_server/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/angusbean/grpc-go-course/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	greetpb.UnimplementedGreetServiceServer
+}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet function was invoked with %v\n", req)
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
+
+func main() {
+	fmt.Println("Server is running")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50052")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
+}
