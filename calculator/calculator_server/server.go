eac0bbc848b6eb4eabb3345edbd7f183a43e9297
angusbean <angus.bean.design@gmail.com>
2021-12-27 15:47:59 +1100

    adding initial files

---
+++ calculator/calculator_server/server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/angusbean/grpc-go-course/calculator/calculatorpb"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	calculatorpb.UnimplementedCalculatorServiceServer
+}
+
+func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
+	fmt.Printf("Sum function was invoked with %v\n", req)
+	firstInt := req.Sum.IntOne
+	secondInt := req.Sum.IntTwo
+
+	result := firstInt + secondInt
+
+	res := &calculatorpb.SumResponse{
+		Result: result,
+	}
+	return res, nil
+
+}
+
+func main() {
+	fmt.Println("Server is running")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50053")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
